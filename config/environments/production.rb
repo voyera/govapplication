b7e631bced08d2d0ec4dd80537f93b1b3be11b18
Alexandre Voyer <Alexandre.Voyer@outlook.com>
2015-02-14 21:31:59 +0000

    Fix new subdomain heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   }
   
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'still-forest-3642.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'govapplication.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
