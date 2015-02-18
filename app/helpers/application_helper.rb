0edad5ba1467bd597171f25b4df56c4a8aec3919
Alexandre Voyer <Alexandre.Voyer@outlook.com>
2015-02-18 15:32:28 +0000

    Implement internalization in page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "Government Application App"
+    base_title = t('base_title')
     if page_title.empty?
       base_title
     else
